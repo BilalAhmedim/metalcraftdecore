3b63877abe57c497981c465f2876f7c7104ae365
Bilal Ahmed <bilalahmedim@gmail.com>
2023-09-10 02:34:06 +0530

    nav/slider/about/gallery  complete.

---
+++ header.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="./../style.css">
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <!-- Your Logo -->
+      <a class="navbar-brand" href="#">
+        <img src="./assets/mcd logo.png" alt="Your Logo" width="100">
+      </a>
+      <!-- /Your Logo -->
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav ml-auto ">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Urns</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Keepsake Urns</a></li>
+              <li><a class="dropdown-item" href="#">Large Urns</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Christmas</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Christmas Bell</a></li>
+              <li><a class="dropdown-item" href="#">Christmass Tree & Deer</a></li>
+              <li><a class="dropdown-item" href="#">Stocking Holder</a></li>
+              <li><a class="dropdown-item" href="#">Wreaths</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Kitchen</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Cutlery</a></li>
+              <li><a class="dropdown-item" href="#">Kitchen Containers</a></li>
+              <li><a class="dropdown-item" href="#">Cake Stand</a></li>
+              <li><a class="dropdown-item" href="#">Alum Bowl & Tray</a></li>
+              <li><a class="dropdown-item" href="#">Cutlery Holder</a></li>
+              <li><a class="dropdown-item" href="#">Wash Mug</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Decorative</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Candle Stand</a></li>
+              <li><a class="dropdown-item" href="#">Flower Vase</a></li>
+              <li><a class="dropdown-item" href="#">Menorah</a></li>
+              <li><a class="dropdown-item" href="#">Sculpture</a></li>
+              <li><a class="dropdown-item" href="#">Wooden items</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Outdoor</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Copper Fire Pit</a></li>
+              <li><a class="dropdown-item" href="#">Planters</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Miscellaneous</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Wine Party Tub</a></li>
+              <li><a class="dropdown-item" href="#">Goblet</a></li>
+              <li><a class="dropdown-item" href="#">Metal Bookend</a></li>
+              <li><a class="dropdown-item" href="#">Watering Can</a></li>
+              <li><a class="dropdown-item" href="#">Table</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
--- index.php
+++ index.php
@@ -1,11 +1,296 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
+<?php include './header.php'; ?>
 
-</body>
-</html>
+<div class="slider">
+  <div id="carouselExampleFade" class="carousel slide carousel-fade carousel-dark" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleFade" data-bs-slide-to="3" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="./assets/slider (1).jpg" class="d-block w-100" alt="..." loading="lazy">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/slider (2).jpg" class="d-block w-100" alt="..." loading="lazy">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/slider (3).jpg" class="d-block w-100" alt="..." loading="lazy">
+      </div>
+      <div class="carousel-item">
+        <img src="./assets/slider (4).jpg" class="d-block w-100" alt="..." loading="lazy">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+</div>
+
+<section class="about">
+  <div class="header">
+    <h1>About Metal Craft Decore</h1>
+  </div>
+  <div class="container">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quidem debitis! Tempore iure atque doloribus nihil
+      totam dolore similique rem sapiente voluptates soluta pariatur, excepturi dicta, sint reiciendis accusantium
+      laborum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quidem debitis! Tempore iure atque doloribus nihil
+      totam dolore similique rem sapiente voluptates soluta pariatur, excepturi dicta, sint reiciendis accusantium
+      laborum?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quidem debitis! Tempore iure atque doloribus nihil
+      totam dolore similique rem sapiente voluptates soluta pariatur, excepturi dicta, sint reiciendis accusantium
+      laborum?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quidem debitis! Tempore iure atque doloribus nihil
+      totam dolore similique rem sapiente voluptates soluta pariatur, excepturi dicta, sint reiciendis accusantium
+      laborum?
+    </p>
+    <button class="btn btn-primary"><a href="/about.php">About Us.</a></button>
+  </div>
+</section>
+
+<section class="gallery">
+  <div class="header">
+    <h1>Metal Craft Decore Products</h1>
+  </div>
+  <div class="container d-grid">
+
+    <div class="card">
+      <a href="./pages/Keepsake Urns.php">
+        <img src="./assets/Urns/Keepsake Urns/1 (1).jpg" class="card-img-top" alt="Keepsake Urns" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Keepsake Urns</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Large Urns.php">
+        <img src="./assets/Urns/Large Urns/1 (1).jpg" class="card-img-top" alt="Large Urns" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Large Urns</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Christmas Bell.php">
+        <img src="./assets/Christmas Decor/Christmas Bell/1 (1).jpg" class="card-img-top" alt="Christmas Bell"
+          loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Christmas Bell</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Kitchenn Container.php">
+        <img src="./assets/Christmas Decor/Christmass Tree & Deer/1 (1).jpg" class="card-img-top"
+          alt="Christmass Tree & Deer" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Christmass Tree & Deer</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Stocking Holder.php">
+        <img src="./assets/Christmas Decor/Stocking Holder/1 (1).jpg" class="card-img-top" alt="Stocking Holders"
+          loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Stocking Holders</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Wreaths.php">
+        <img src="./assets/Christmas Decor/Wreaths/1 (1).jpg" class="card-img-top" alt="Wreaths" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Wreaths</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Cutlery.php">
+        <img src="./assets/Cutlery/1 (1).jpg" class="card-img-top" alt="Cutlery" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Cutlery</h5>
+        </div>
+      </a>
+    </div>
+
+
+    <div class="card">
+      <a href="Kitchenn Container.php">
+        <img src="./assets/Kitchen Container/1 (1).jpg" class="card-img-top" alt="Kitchen Containers" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Kitchen Containers</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Cake Stand.php">
+        <img src="./assets/Cake Stand/1 (1).jpg" class="card-img-top" alt="Cake Stands" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Cake Stands</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Alum Bowl & Tray.php">
+        <img src="./assets/Alum Bowl & Tray/1 (1).jpg" class="card-img-top" alt="Alum Bowl & Trays" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Alum Bowl & Trays</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Cutlery Holder.php">
+        <img src="./assets/Cutlery Holder/1 (1).jpg" class="card-img-top" alt="Cutlery Holders" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Cutlery Holders</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Wash Mug.php">
+        <img src="./assets/Wash Mug/1 (1).jpg" class="card-img-top" alt="Wash Mugs" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Wash Mugs</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Candle Stand.php">
+        <img src="./assets/Candle Stand/1 (1).jpg" class="card-img-top" alt="Candle Stands" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Candle Stands</h5>
+        </div>
+      </a>
+    </div>
+
+
+    <div class="card">
+      <a href="Menorah.php">
+        <img src="./assets/Menorah/1 (1).jpg" class="card-img-top" alt="Menorahs" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Menorahs</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Sculpture.php">
+        <img src="./assets/Sculpture/1 (1).jpg" class="card-img-top" alt="Sculptures" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Sculptures</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Wooden items.php">
+        <img src="./assets/Wooden items/1 (1).jpg" class="card-img-top" alt="Wooden items" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Wooden items</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Copper Fire Pit.php">
+        <img src="./assets/Copper Fire Pit/1 (1).jpg" class="card-img-top" alt="Copper Fire Pits" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Copper Fire Pits</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Hanging Planter.php">
+        <img src="./assets/Planters/Hanging Planter/1 (1).jpg" class="card-img-top" alt="Hanging Planter"
+          loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Hanging Planter</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="planters with Stands.php">
+        <img src="./assets/Planters/planters with Stands/1 (1).jpg" class="card-img-top" alt="planters with Stands"
+          loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">planters with Stands</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Wine Party Tub.php">
+        <img src="./assets/Wine Party Tub/1 (1).jpg" class="card-img-top" alt="Wine Party Tubs" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Wine Party Tubs</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Goblet.php">
+        <img src="./assets/Goblet/1 (1).jpg" class="card-img-top" alt="Goblets" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Goblets</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Metal Bookend.php">
+        <img src="./assets/Metal Bookend/1 (1).jpg" class="card-img-top" alt="Metal Bookends" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Metal Bookends</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Watering Can.php">
+        <img src="./assets/Watering Can/1 (1).jpg" class="card-img-top" alt="Watering Cans" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Watering Cans</h5>
+        </div>
+      </a>
+    </div>
+
+    <div class="card">
+      <a href="Table.php">
+        <img src="./assets/Table/1 (1).jpg" class="card-img-top" alt="Tables" loading="lazy">
+        <div class="card-body">
+          <h5 class="card-title">Tables</h5>
+        </div>
+      </a>
+    </div>
+
+
+  </div>
+</section>
+
+<?php include './footer.php'; ?>
