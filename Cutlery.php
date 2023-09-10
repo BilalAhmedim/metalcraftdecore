f773fb788e75e4db65deb81760397e1a9b948a5c
Bilal Ahmed <bilalahmedim@gmail.com>
2023-09-11 04:47:18 +0530

    gallery page working fine.

--- Cutlery.php
+++ Cutlery.php
@@ -1,27 +1,79 @@
-<?php include './header.php';
+<?php include 'header.php';
+$page = 'Cutlery';
+$count = 1;
+$count_max = 20;
+$path = './assets/Cutlery';
 
-$image_name = 1;
 ?>
 
+
 <section>
-  <div class="contain">
-    <h1></h1>
+  <div class="container">
+    <h1>gallery</h1>
   </div>
-  <div class="container d-grid">
-    <!-- <?php
-while ($image_name == 20) {
+  <div class="container gallery-js d-grid">
 
+<?php
+while ($count <= $count_max) {
+  ?>
+
+    <a href="<?php echo $path . '/1 (' . $count . ').jpg'; ?>"
+    data-pswp-width="2000" data-pswp-height="2000"
+      data-cropped="true" target="_blank">
+      <img src="<?php echo $path . '/1 (' . $count . ').jpg'; ?>"
+      alt="<?php echo $page; ?>"
+      class="img-fluid" loading="lazy"/>
+      <div class="caption"><?php echo $page . '#' . $count; ?></div>
+    </a>
+
+<?php
+$count++;
 }
-?> -->
-
-
-
-
-
+?>
   </div>
 </section>
 
+<script type="module">
+  import PhotoSwipeLightbox from 'https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.3/photoswipe-lightbox.esm.min.js';
 
+  const options = {
+    //Any element with this class will become an individual gallery
+    gallery: '.gallery-js',
 
+    // Your slides will be every <a> tag that is children from .gallery-js
+    children: 'a',
+    // This module will only load when you click to show the images, making your data bundle lighter
+    pswpModule: () => import('https://cdnjs.cloudflare.com/ajax/libs/photoswipe/5.3.3/photoswipe.esm.min.js')
+  };
 
-<?php include './footer.php';?>
+  const lightbox = new PhotoSwipeLightbox(options);
+lightbox.on('uiRegister', function() {
+  lightbox.pswp.ui.registerElement({
+    name: 'custom-caption',
+    order: 9,
+    isButton: false,
+    appendTo: 'root',
+    html: 'Caption text',
+    onInit: (el, pswp) => {
+      lightbox.pswp.on('change', () => {
+        const currSlideElement = lightbox.pswp.currSlide.data.element;
+        let captionHTML = '';
+        if (currSlideElement) {
+          const hiddenCaption = currSlideElement.querySelector('.caption');
+          if (hiddenCaption) {
+            // get caption from element with class hidden-caption-content
+            captionHTML = hiddenCaption.innerHTML;
+          } else {
+            // get caption from alt attribute
+            captionHTML = currSlideElement.querySelector('img').getAttribute('alt');
+          }
+        }
+        el.innerHTML = captionHTML || '';
+      });
+    }
+  });
+});
+lightbox.init();
+
+</script>
+<?php include 'footer.php';?>
