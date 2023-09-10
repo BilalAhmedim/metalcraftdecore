1960c6825b047236b415a20b3b11dd3c71d18257
Bilal Ahmed <bilalahmedim@gmail.com>
2023-09-11 03:13:35 +0530

    nav saperated.

---
+++ Cutlery.php
@@ -0,0 +1,27 @@
+<?php include './header.php';
+
+$image_name = 1;
+?>
+
+<section>
+  <div class="contain">
+    <h1></h1>
+  </div>
+  <div class="container d-grid">
+    <!-- <?php
+while ($image_name == 20) {
+
+}
+?> -->
+
+
+
+
+
+  </div>
+</section>
+
+
+
+
+<?php include './footer.php';?>
---
+++ nav.php
@@ -0,0 +1,95 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <!-- Your Logo -->
+      <a class="navbar-brand" href="#">
+        <img src="/assets/mcd logo.png" alt="Your Logo" width="100">
+      </a>
+      <!-- /Your Logo -->
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav ml-auto ">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Urns</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Keepsake Urns</a></li>
+              <li><a class="dropdown-item" href="#">Large Urns</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Christmas</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Christmas Bell</a></li>
+              <li><a class="dropdown-item" href="#">Christmass Tree & Deer</a></li>
+              <li><a class="dropdown-item" href="#">Stocking Holder</a></li>
+              <li><a class="dropdown-item" href="#">Wreaths</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Kitchen</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Cutlery</a></li>
+              <li><a class="dropdown-item" href="#">Kitchen Containers</a></li>
+              <li><a class="dropdown-item" href="#">Cake Stand</a></li>
+              <li><a class="dropdown-item" href="#">Alum Bowl & Tray</a></li>
+              <li><a class="dropdown-item" href="#">Cutlery Holder</a></li>
+              <li><a class="dropdown-item" href="#">Wash Mug</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Decorative</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Candle Stand</a></li>
+              <li><a class="dropdown-item" href="#">Flower Vase</a></li>
+              <li><a class="dropdown-item" href="#">Menorah</a></li>
+              <li><a class="dropdown-item" href="#">Sculpture</a></li>
+              <li><a class="dropdown-item" href="#">Wooden items</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Outdoor</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Copper Fire Pit</a></li>
+              <li><a class="dropdown-item" href="#">Hanging Planter</a></li>
+              <li><a class="dropdown-item" href="#">planters with Stands</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+              aria-expanded="false">Miscellaneous</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Wine Party Tub</a></li>
+              <li><a class="dropdown-item" href="#">Goblet</a></li>
+              <li><a class="dropdown-item" href="#">Metal Bookend</a></li>
+              <li><a class="dropdown-item" href="#">Watering Can</a></li>
+              <li><a class="dropdown-item" href="#">Table</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
